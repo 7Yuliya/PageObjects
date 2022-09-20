27ea330f31772c76d2800396a2c3f48d2472eb82
Юлия Алексина <uliyaalexina@mail.ru>
2022-09-20 02:07:34 -0700

    Fixed src/

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -65,13 +65,6 @@ public class MoneyTransferTest {
         assertEquals(balance2 + sum, endBalance2);
     }
 
-    @Test
-    @DisplayName("Не должен переводить сумму,превышающую лимит карты")
-    void shouldNotTransferMoneyMoreThanCardLimit() {
-        sum = balance1 + 200;
-        val page = dashboardPage.click(dashboardPage.card2);
-        val card = DataHelper.getFirstCard().getNumber();
-        page.unsuccessful(Integer.toString(sum), card);
-    }
+
 }
 
